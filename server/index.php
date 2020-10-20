d164d8cfd3490ec43305c51e44c6e75b15848ac3
Pablo Cordero <pablocorderoromero@gmail.com>
2020-10-20 20:13:28 +0200

    Arreglado el problema de twig

--- server/index.php
+++ server/index.php
@@ -2,6 +2,10 @@
     require_once "vendor/autoload.php";
     include('funciones.php');
 
+    // ini_set('display_errors', 1);
+    // ini_set('display_startup_errors', 1);
+    // error_reporting(E_ALL);
+
     $loader = new \Twig\Loader\FilesystemLoader('templates');
     $twig = new \Twig\Environment($loader);
     //Conexion a la base de datos
--- subir.sh
+++ subir.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 HOST=dgp.esy.es
 USER=u681824297.$1_ftp
-PASSWD=dgp.7.host.FTP
-ncftp -u $USER -p $PASSWD $HOST <<EOF
-rm -r *
-put server/*
+PASSWD_FTP=dgp.7.host.FTP
+PASSWD_SSH=dgp.7.host.a
+
+sshpass -f <(printf '%s\n' $PASSWD_SSH) ssh -p 65002 u681824297@31.220.20.85 "cd public_html/$1/ ; rm -r *"
+
+ncftp -u $USER -p $PASSWD_FTP $HOST <<EOF
+put -R server/*
 EOF
+
 exit 0

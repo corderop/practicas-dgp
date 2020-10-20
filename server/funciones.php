74ba934aa9cd92f8b8cfb2848619e52020bcc53b
marinahbau <58307330+marinahbau@users.noreply.github.com>
2020-10-20 17:47:20 +0200

    Creado fichero de funciones

---
+++ server/funciones.php
@@ -0,0 +1,18 @@
+<?php
+
+    function conectar_bd(){
+        $mysqli = new mysqli("localhost", "u681824297_dgp", "dgp.7.host.DB", "u681824297_dgp");
+        if ($mysqli->connect_errno) {
+            echo ("Fallo al conectar: " . $mysqli->connect_error);
+        }
+        else {
+            echo("funciona");
+        }
+        return $mysqli;
+    }
+
+    function 
+
+
+
+?>

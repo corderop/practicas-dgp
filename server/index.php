13e672802a6049e5421ea6fe037ff2fdfb0e029d
marinahbau <58307330+marinahbau@users.noreply.github.com>
2020-10-20 18:45:10 +0200

    Inicio twig(no funciona)

--- server/index.php
+++ server/index.php
@@ -1,10 +1,13 @@
 <?php
-    require __DIR__ . '/vendor/autoload.php';
+    require_once "vendor/autoload.php";
     include('funciones.php');
 
     $loader = new \Twig\Loader\FilesystemLoader('templates');
     $twig = new \Twig\Environment($loader);
     //Conexion a la base de datos
     $mysqli=conectar_bd();
+
+    echo $twig->render('prueba.html');
+
 
 ?>

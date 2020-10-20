8abca519b3f1b1c3e42004a4f05010ef68c5d6ae
Pablo Cordero <pablocorderoromero@gmail.com>
2020-10-20 20:21:43 +0200

    twig funcionando

--- server/index.php
+++ server/index.php
@@ -1,10 +1,6 @@
 <?php
-    require_once "vendor/autoload.php";
+    require_once "../vendor/autoload.php";
     include('funciones.php');
-
-    // ini_set('display_errors', 1);
-    // ini_set('display_startup_errors', 1);
-    // error_reporting(E_ALL);
 
     $loader = new \Twig\Loader\FilesystemLoader('templates');
     $twig = new \Twig\Environment($loader);

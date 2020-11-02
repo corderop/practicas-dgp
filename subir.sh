2f12d252d6443ef36e109187672d096ed49a829f
Pablo Cordero <pablocorderoromero@gmail.com>
2020-11-02 18:17:09 +0100

    Arreglado fallo del script de subida, nueva forma de uso (Leer readme)

--- subir.sh
+++ subir.sh
@@ -11,7 +11,10 @@ USER=u681824297.$1_ftp
 PASSWD_FTP=dgp.7.host.FTP
 PASSWD_SSH=dgp.7.host.a
 
-sshpass -f <(printf '%s\n' $PASSWD_SSH) ssh -p 65002 u681824297@31.220.20.85 "cd public_html/$1/ ; rm -r *"
+if [ ! -v $2 ]
+then
+    sshpass -f <(printf '%s\n' $PASSWD_SSH) ssh -p 65002 u681824297@31.220.20.85 "cd domains/dgp.esy.es/public_html/$1/ ; rm -r *"
+fi
 
 ncftp -u $USER -p $PASSWD_FTP $HOST <<EOF
 put -R server/*

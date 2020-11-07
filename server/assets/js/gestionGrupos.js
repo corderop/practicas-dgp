cd4069e1f114badf419fdea0e2773871ce10da7c
Pablo Cordero <pablocorderoromero@gmail.com>
2020-11-07 21:01:55 +0100

    Crear un grupo nuevo funcionando al completo

--- server/assets/js/gestionGrupos.js
+++ server/assets/js/gestionGrupos.js
@@ -25,3 +25,46 @@ function quitarUsuarioGrupo(el, cod_user){
     // Cambia la función onclick
     el.setAttribute('onclick', `aniadirUsuarioGrupo(this, ${cod_user})`);
 }
+
+// Cuando se pulsa submit se comprueba que hay un nombre y al menos 1 usuario en el grupo
+// y se crean los input para los usuarios para que los detecte PHP como un array
+function hacerSubmit(){
+    var nombre = document.getElementById('nombre-grupo').value;
+    var warning = document.getElementById('warning-mensaje');
+    var lista = document.getElementById('lista-errores');
+
+    // Entra si el nombre no es correcto o no se han seleccionado usuarios
+    if(!nombre || !usuariosGrupos.length){ 
+        warning.style.display = "block";
+        lista.innerHTML = "";
+
+        if(!nombre){
+            var li = document.createElement('li');
+            li.innerHTML = "Introducir un nombre";
+            lista.appendChild(li);
+        }
+
+        if(!usuariosGrupos.length){
+            var li = document.createElement('li');
+            li.innerHTML = "Añadir al menos un usuario al grupo";
+            lista.appendChild(li);
+        }
+
+        // returnToPreviousPage();
+        return false;
+    }
+
+    // Añade los usuarios como input para que los detecte PHP
+    let formulario = document.getElementById('modificar-grupo');
+    
+    for(i=0; i<usuariosGrupos.length; i++){
+        var input = document.createElement("input");
+        var user = formulario.appendChild(input);
+        user.type = 'text';
+        user.setAttribute("value",`${usuariosGrupos[i]}`);
+        user.name = `usuarios[]`;
+        user.style.display = "none";
+    }
+
+    return true;
+}

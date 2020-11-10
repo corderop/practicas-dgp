2c7be64592297706c64cfe352b85d5b560a31963
Pablo Cordero <pablocorderoromero@gmail.com>
2020-11-10 18:45:25 +0100

    Busqueda adaptada para la sección de usuarios al añadir grupo

--- server/assets/js/funciones.js
+++ server/assets/js/funciones.js
@@ -1,19 +1,26 @@
-function buscar(busq){
+/**
+ * Filtra a las tarjetas en base a lo introducido en la barra de búsqueda
+ * @param {*} busq Objeto input en html. Usar el parámetro this al llamarla en HTML.
+ * @param {*} num Tarjeta en la que quieres que empiece. Normalmente 0. Será 1 en las ocasiones
+ * en las que exista una tarjeta que sirva para añadir tareas etc.
+ */
+function buscar(busq, num){
     // Toma el valor introducido en la barra
     let valor = busq.value.toLowerCase();
     // Toma las tarjetas
     let tarjetas = document.querySelectorAll('.ui .card');
 
     if(valor.length > 3){
-        for(i=1; i<tarjetas.length; i++){
+        for(i=num; i<tarjetas.length; i++){
             if(tarjetas[i].textContent.trim().toLowerCase().includes(valor))
                 tarjetas[i].style.display = "";
             else
                 tarjetas[i].style.display = "none";
         }
     }
+    // Para que se muestren todas las tarjetas cuando borra la búsqueda
     else{
-        for(i=1; i<tarjetas.length; i++){
+        for(i=num; i<tarjetas.length; i++){
             tarjetas[i].style.display = "";
         }
     }

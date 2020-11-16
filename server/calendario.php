8aac55128d09d4284c4dee6f4bec52bb20061587
Pablo Cordero <pablocorderoromero@gmail.com>
2020-11-16 19:56:45 +0100

    Calendario para el tutor funcionando al completo

---
+++ server/calendario.php
@@ -0,0 +1,31 @@
+<?php
+    require_once "../vendor/autoload.php";
+    include('funciones.php');
+
+    $loader = new \Twig\Loader\FilesystemLoader('templates');
+    $twig = new \Twig\Environment($loader);
+    //Conexion a la base de datos
+    $mysqli=conectar_bd();
+
+    session_start();
+    if(isset($_SESSION['cod_usuario'])){
+        $usuario=getUsuario($mysqli, $_SESSION['cod_usuario']);
+        
+        if($usuario['tipo'] == "ADMIN"){
+            header("Location: index.php");
+        }
+        if($usuario['tipo'] == "TUTOR"){
+            $tareas = getTareasTutor($mysqli, $_SESSION['cod_usuario']);
+            echo $twig->render('calendario.html', ['usuario'=>$usuario, 'tareas'=>$tareas, 'titulo'=>"Calendario"]);
+        }
+        if($usuario['tipo'] == "USUARIO"){
+            header("Location: index.php");
+        }
+    }
+    else{
+        echo $twig->render('login.html');
+    }
+   
+
+
+?>

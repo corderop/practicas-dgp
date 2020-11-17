6d50559c4b19e15cd7bd355ba5a7e543e1fa3013
Pablo Cordero <pablocorderoromero@gmail.com>
2020-11-17 19:13:19 +0100

    Vista de tarea mejorada: - Se muestra los archivos multimedia de todos los tipos y tamaños - Se cargan los mensajes de texto en el chat (no los multimedia aún)

--- server/tarea.php
+++ server/tarea.php
@@ -15,18 +15,19 @@
         if(isset($_GET['cod_tarea'])){
             $cod_tarea = $_GET['cod_tarea'];
             $tarea = getTarea($mysqli, $cod_tarea);
+            $tipo = mime_content_type($tarea['multimedia']);
 
             // Cargar la tarea
             if($usuario['tipo'] == "TUTOR"){
-                echo $twig->render('tarea.html', ['usuario'=>$usuario, 'tarea'=>$tarea, 'titulo'=>$tarea['titulo']]);
+                echo $twig->render('tarea.html', ['usuario'=>$usuario, 'tarea'=>$tarea, 'tipo'=>$tipo, 'titulo'=>$tarea['titulo']]);
             }
             if($usuario['tipo'] == "USUARIO"){
-                echo $twig->render('tarea.html', ['usuario'=>$usuario, 'tarea'=>$tarea, 'titulo'=>$tarea['titulo']]);
+                echo $twig->render('tarea.html', ['usuario'=>$usuario, 'tarea'=>$tarea, 'tipo'=>$tipo, 'titulo'=>$tarea['titulo']]);
             }
         }
         else{
             // Si no se ha indicado ninguna tarea se redirecciona al inicio
-            // header("Location: index.php");
+            header("Location: index.php");
         }        
     }
     else{

d74e0631c21d540d4c83e76b74184caefacc7d14
daniel5neron <daniel5neron@gmail.com>
2020-11-20 17:21:41 +0100

    Comienzo pruebas con tareas desde APP

---
+++ server/app/tareas.php
@@ -0,0 +1,24 @@
+<?php
+    require_once "../../vendor/autoload.php";
+    include('../funciones.php');
+
+    //Conexion a la base de datos
+    $mysqli=conectar_bd();
+
+    $body = file_get_contents('php://input');
+    $usuario = json_decode($body);
+
+    $cod_usuario = $usuario->cod_usuario;
+
+    $tareas[] = getTareas($mysqli, $cod_usuario);
+
+    if (!$tareas[].empty()) {
+        http_response_code(201);
+        $respuesta = json_encode(array($tareas));
+        echo $respuesta;
+    } else {
+        http_response_code(401);
+        $respuesta["error"] = "no existen tareas para ese usuario";
+        return ["estado" => 1, "usuario" => $respuesta];
+    }
+?>

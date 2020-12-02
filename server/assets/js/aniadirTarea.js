ae4c41b114111930121e3b1355d9ba395e98eb0f
Pablo Cordero <pablocorderoromero@gmail.com>
2020-12-02 21:06:06 +0100

    Fecha por usuario en añadir tarea. Front-end hecho, falta back-end

--- server/assets/js/aniadirTarea.js
+++ server/assets/js/aniadirTarea.js
@@ -1,5 +1,7 @@
 let usuarios = [];
+let fechas_usuarios = [];
 let grupos = [];
+let fechas_grupos = [];
 
 // ********************************
 // Añadir y quitar usuarios
@@ -9,9 +11,11 @@ function aniadirUsuario(el, cod_user){
     // Vuelve el boton azul y pone añadido
     el.classList.remove('basic');
     el.innerHTML = "Añadido";
+    fecha = document.getElementById(`fecha-usuario-${cod_user}`);
 
     // Añade a la lista el usuario
     usuarios.push(cod_user);
+    fechas_usuarios.push(fecha.value);
 
     // Cambia la función onclick
     el.setAttribute('onclick', `quitarUsuario(this, ${cod_user})`);
@@ -24,11 +28,21 @@ function quitarUsuario(el, cod_user){
 
     // Quita de la lista el usuario
     var index = usuarios.indexOf(cod_user);
-    if (index >= 0)
+    if (index >= 0){
         usuarios.splice( index, 1 );
+        fechas_usuarios.splice( index, 1);
+    }
 
     // Cambia la función onclick
     el.setAttribute('onclick', `aniadirUsuario(this, ${cod_user})`);
+}
+
+function cambiarFechaUsuario(el, cod_user){
+    console.log(`fecha cambiada user ${cod_user}`);
+    var index = usuarios.indexOf(cod_user);
+    if (index >= 0){
+        fechas_usuarios[index] = el.value;
+    }
 }
 
 // ********************************
@@ -40,9 +54,11 @@ function aniadirGrupo(el, cod_grupo){
     // Vuelve el boton azul y pone añadido
     el.classList.remove('basic');
     el.innerHTML = "Añadido";
+    fecha = document.getElementById(`fecha-grupo-${cod_grupo}`);
 
     // Añade a la lista el grupo
     grupos.push(cod_grupo);
+    fechas_grupos.push(fecha.value);
 
     // Cambia la función onclick
     el.setAttribute('onclick', `quitarGrupo(this, ${cod_grupo})`);
@@ -55,11 +71,21 @@ function quitarGrupo(el, cod_grupo){
 
     // Añade a la lista del grupo el usuario
     var index = grupos.indexOf(cod_grupo);
-    if (index >= 0)
+    if (index >= 0){
         grupos.splice( index, 1 );
+        fechas_grupos.splice( index, 1);
+    }
 
     // Cambia la función onclick
     el.setAttribute('onclick', `aniadirGrupo(this, ${cod_grupo})`);
+}
+
+function cambiarFechaGrupo(el, cod_grupo){
+    console.log(`fecha cambiada grupo ${cod_grupo}`);
+    var index = grupos.indexOf(cod_grupo);
+    if (index >= 0){
+        fechas_grupos[index] = el.value;
+    }
 }
 
 
@@ -70,12 +96,13 @@ function hacerSubmit(){
     var comentario = document.getElementById('comentario-tarea').value;
     var warning = document.getElementById('warning-mensaje');
     var lista = document.getElementById('lista-errores');
+    lista.innerHTML = "";
+    warning.style.display = "none";
 
     // Entra si no se ha introducido nombre, no se ha introducido comentario
     // o no se ha introducio ningún usuario y ningún grupo
     if(!nombre || !comentario || (!usuarios.length && !grupos.length)){
         warning.style.display = "block";
-        lista.innerHTML = "";
 
         if(!nombre){
             var li = document.createElement('li');
@@ -111,6 +138,27 @@ function hacerSubmit(){
         user.style.display = "none";
     }
 
+    //Añade las fechas de los usuarios
+    for(i=0; i<fechas_usuarios.length; i++){
+        if(fechas_usuarios[i] == ""){
+            cambiarFechaUsuario(document.getElementById(`fecha-usuario-${usuarios[i]}`), usuarios[i]);
+            if(fechas_usuarios[i] == ""){
+                warning.style.display = "block";
+                var li = document.createElement('li');
+                li.innerHTML = "Hay usuarios que no tienen fecha asignada";
+                lista.appendChild(li);
+
+                return false;
+            }
+        }
+
+        var input = document.createElement("input");
+        var user = formulario.appendChild(input);
+        user.type = 'text';
+        user.setAttribute("value",`${fechas_usuarios[i]}`);
+        user.name = `fechas_usuarios[]`;
+        user.style.display = "none";
+    }
 
     // Añade los grupos
     for(i=0; i<grupos.length; i++){
@@ -121,6 +169,28 @@ function hacerSubmit(){
         user.name = `grupos[]`;
         user.style.display = "none";
     }    
+
+    //Añade las fechas de los grupos
+    for(i=0; i<fechas_grupos.length; i++){
+        if(fechas_grupos[i] == ""){
+            cambiarFechaGrupo(document.getElementById(`fecha-grupo-${grupos[i]}`), grupos[i]);
+            if(fechas_grupos[i] == ""){
+                warning.style.display = "block";
+                var li = document.createElement('li');
+                li.innerHTML = "Hay grupos que no tienen fecha asignada";
+                lista.appendChild(li);
+
+                return false;
+            }
+        }
+
+        var input = document.createElement("input");
+        var user = formulario.appendChild(input);
+        user.type = 'text';
+        user.setAttribute("value",`${fechas_grupos[i]}`);
+        user.name = `fechas_grupos[]`;
+        user.style.display = "none";
+    }
 
     document.getElementById('warning-carga').style.display = "flex";
 

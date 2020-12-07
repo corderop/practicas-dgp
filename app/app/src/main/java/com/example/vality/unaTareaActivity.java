97cd3e70adc43b21629407a796da75403f4cda44
daniel5neron <daniel5neron@gmail.com>
2020-12-08 00:29:10 +0100

    Pruebas reproducción multimedia desde APP

--- app/app/src/main/java/com/example/vality/unaTareaActivity.java
+++ app/app/src/main/java/com/example/vality/unaTareaActivity.java
@@ -1,13 +1,26 @@
 package com.example.vality;
 
+import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -43,5 +56,83 @@ public class unaTareaActivity extends AppCompatActivity {
         setContentView(R.layout.pantalla_tarea);
         TextView titulo = this.findViewById(R.id.tituloTarea);
         titulo.setText(this.tarea.getTitulo());
+
+        if(tarea.getMultimedia().endsWith("webp") || tarea.getMultimedia().endsWith("png") || tarea.getMultimedia().endsWith("jpg") || tarea.getMultimedia().endsWith("jpeg")){
+            cargarImagen();
+        }else{
+            if(tarea.getMultimedia().endsWith("mp4")){
+                cargarVideo();
+            }else{
+                if(tarea.getMultimedia().endsWith("ogg")) {
+                    cargarAudio();
+                }
+            }
+        }
+    }
+
+    private void cargarImagen(){
+        String url="http://test.dgp.esy.es/"+ tarea.getMultimedia();
+        ImageView imagen = this.findViewById(R.id.imagenTarea);
+        imagen.setVisibility(View.VISIBLE);
+
+        System.out.println("Creamos mensaje para pedir imagen: "+ tarea.getMultimedia());
+
+        ImageRequest imageRequest = new ImageRequest(url,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        imagen.setImageBitmap(response);
+                    }
+                }, 0, 0, ImageView.ScaleType.CENTER, null, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("ERROR: "+ error);
+            }
+        });
+        queue.add(imageRequest);
+    }
+
+    private void cargarVideo() {
+        VideoView videoView =(VideoView)findViewById(R.id.videoTarea);
+        videoView.setVisibility(View.VISIBLE);
+
+        System.out.println("Creamos mensaje para pedir video: " + tarea.getMultimedia());
+
+        videoView.setVideoURI(Uri.parse("http://test.dgp.esy.es/"+ tarea.getMultimedia()));
+        videoView.requestFocus();
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+            }
+        });
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                System.out.println("Iniciamos video");
+                videoView.start();
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+                        MediaController mediaController = new MediaController(unaTareaActivity.this);
+                        videoView.setMediaController(mediaController);
+                        mediaController.setAnchorView(videoView);
+                    }
+                });
+            }
+        });
+
+        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+                System.out.println("Ha ocurrido un problema con el vídeo");
+                return false;
+            }
+        });
+    }
+
+    private void cargarAudio(){
+
     }
 }

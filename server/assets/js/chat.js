dd9d17558ccfd40f44cc137e067024972b49140e
Pablo Cordero <pablocorderoromero@gmail.com>
2020-12-08 11:23:58 +0100

    Accesibilidad completa y arreglado pequeño fallo del calendario del usuario

--- server/assets/js/chat.js
+++ server/assets/js/chat.js
@@ -3,7 +3,6 @@ function hacerSubmitChat(){
     var warning = document.getElementById('warning-mensaje');
     var archivos = document.getElementById('multimedia-envio').files.length;
 
-    // Entra si el nombre no es correcto o no se han seleccionado usuarios
     if(!nombre && !archivos){ 
         warning.style.display = "block";
 
